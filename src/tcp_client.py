f9307765aadc197006d50ed33f45ac720361ad45
ingjae <hij1995@naver.com>
2021-02-03 23:47:01 +0900

    Update : TCP Server and move Client #working code

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -3,40 +3,55 @@
 import time
 import socket
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String,Bool
  
-
+## move client 
 class TCPListener(object):
     def __init__(self):
-        self.odom_sub = rospy.Subscriber('tcptopic', String, self.tcpCallback)
+        self.tcp_sub = rospy.Subscriber('tcptopic', String, self.tcpCallback)
         self.tcp_msg = String()
-
+        self.finish_pub = rospy.Publisher('is_finished',Bool,queue_size=10)
         self.rate = rospy.Rate(1.0)
         self.once = False
+        self.is_finished = False
 
     def tcpCallback(self, msg):
         self.tcp_msg = msg
 
+    def checkPublisher(self):
+        self.is_finished = True
+        self.finish_pub.publish(self.is_finished)
+        self.is_finished = False
 
 if __name__=="__main__":    
     try:
         rospy.init_node('tcp_listener')
-        rate = rospy.Rate(1)
+        rate = rospy.Rate(10)
         tcp = TCPListener()
         while not rospy.is_shutdown():
             # print(tcp.tcp_msg)
             if tcp.tcp_msg.data == "00000001":
                 print("Moving Out Position")
+                time.sleep(5)# robot arm moving
+                tcp.checkPublisher()
             elif tcp.tcp_msg.data == "00000002":
                 print("QR Position and Calculate TF")
+                time.sleep(5)# robot arm moving
+                tcp.checkPublisher()
             elif tcp.tcp_msg.data == "00000004":
                 print("Charging Position")
+                time.sleep(5)# robot arm moving
+                tcp.checkPublisher()
             elif tcp.tcp_msg.data == "00000003":
                 print("Waiting Position")
+                time.sleep(5)# robot arm moving
+                tcp.checkPublisher()
             elif tcp.tcp_msg.data == "00000005":
                 print("Movig In Position")
+                time.sleep(5)# robot arm moving
+                tcp.checkPublisher()
             elif tcp.tcp_msg.data == "":
-                print("Waiting Command")
+                pass
             else:
                 print("Error")
             tcp.tcp_msg.data ="" 
--- src/tcp_server.py
+++ src/tcp_server.py
@@ -3,39 +3,59 @@
 import time
 import socket
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String,Bool
+
+class TCPServer(object):
+    def __init__(self):
+        self.HOST='192.168.1.100'
+        self.PORT=8008
+        self.BUFFER=4096
+        self.RATE = rospy.Rate(10)
+        self.pub=rospy.Publisher('tcptopic',String,queue_size=10)
+        self.sub = rospy.Subscriber('is_finished', Bool,self.checkCallback)
+        self.check = False
+
+    def checkCallback(self, msg):
+        self.check = msg.data
+
+
+if __name__=="__main__":    
+    try:
+        rospy.init_node('tcpserver',anonymous=0)
+        tcp_server = TCPServer()
+        serverSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        serverSock.bind((tcp_server.HOST,tcp_server.PORT))
+        serverSock.listen(5)
+        print 'TCP Server Activate'
+
+        connectionSock,addr=serverSock.accept() #waiting connect
+        print 'TCP Server Connected'
+        
+        while not rospy.is_shutdown():
+            try :
+                # connectionSock.settimeout(5)
+                buffer=connectionSock.recv(tcp_server.BUFFER)
+                # print buf
+                tcp_server.pub.publish(buffer)
+                time.sleep(6)
+                if tcp_server.check == True:
+                    connectionSock.send('00000010')
+                    tcp_server.check = False
+                else:
+                    connectionSock.send('00000011')
+                    
+            except socket.timeout:
+                print 'time out'
+            
+            tcp_server.RATE.sleep()
+        print "exit"
+        connectionSock.close()
+    except Exception as e:
+        rospy.logerr(e)
+    finally:
+        pass
+
+
+
+
  
-######################tcp begining
-HOST='192.168.1.100'
-# HOST = socket.gethostbyname("localhost")
-print HOST
-PORT=8008
- 
-BUFFER=4096
- 
-serverSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
- 
-serverSock.bind((HOST,PORT))
- 
-serverSock.listen(5)
- 
-################ros begining
-rospy.init_node('tcptalker',anonymous=0)
-pub=rospy.Publisher('tcptopic',String,queue_size=10)
- 
-print 'i am listening'
- 
-connectionSock,addr=serverSock.accept() #waiting connect
-while not rospy.is_shutdown():
-    
-    try :
-        connectionSock.settimeout(5)
-        buf=connectionSock.recv(BUFFER)
-        # print buf
-        pub.publish(buf)
-        time.sleep(1)
-    except socket.timeout:
-        print 'time out'
-    connectionSock.send('yes i recv')
-print "exit"
-connectionSock.close()
