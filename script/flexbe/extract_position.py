4ba55f97b6a2c593762da30b7a32577f1ca86edf
ingjae <hij1995@naver.com>
2021-03-21 19:01:04 +0900

    Add : jointstates에서 position만 추출

--- script/flexbe/extract_position.py
+++ script/flexbe/extract_position.py
@@ -4,8 +4,8 @@ joint_list = []
 line_count =0
 # with open('/home/ingjae/catkin_ws/src/move_arm/trajectory_csv/test.csv') as traj:
 
-data = pd.read_csv(r"/home/ingjae/catkin_ws/src/move_arm/trajectory_csv/jointstate_test.csv")
+data = pd.read_csv(r"/home/ingjae/catkin_ws/src/move_arm/trajectory_csv/readypose_100hz.csv")
 position = pd.DataFrame(data, columns=['.position'])
 print (position)
 
-position.to_csv(r"/home/ingjae/catkin_ws/src/move_arm/trajectory_csv/position_test.csv",index=False)
+position.to_csv(r"/home/ingjae/catkin_ws/src/move_arm/trajectory_csv/position_readypose_100hz.csv",index=False)
