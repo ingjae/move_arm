2143d963525325e909cafcc519555aaf91d486c1
ingjae <hij1995@naver.com>
2021-06-07 13:54:52 +0900

    Update : 라떼 판다에  있는 원본

--- src/Clavis_Solutions/clavis_tcp_server.py
+++ src/Clavis_Solutions/clavis_tcp_server.py
@@ -12,7 +12,7 @@ class TCPServer(object):
     def __init__(self):
         self.HOST='192.168.1.200'
         self.PORT=8080
-        self.BUFFER=4096
+        self.BUFFER=1024
         self.RATE = rospy.Rate(10)              
         self.pub=rospy.Publisher('tcptopic',String,queue_size=10)
         self.sub = rospy.Subscriber('is_finished', Bool ,self.finishCallback) 
@@ -52,162 +52,169 @@ class TCPServer(object):
 
 
 
-if __name__=="__main__":    
-    try:
-        rospy.init_node('tcpserver',anonymous=0)
-        tcp_server = TCPServer()
-        serverSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        serverSock.bind((tcp_server.HOST,tcp_server.PORT))
-        serverSock.listen(5)
-        print 'TCP Server Activate'
+if __name__=="__main__":
+    while True:
+        try:
+            rospy.init_node('tcpserver',anonymous=0)
+            tcp_server = TCPServer()
+            serverSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)# To Fix WinError 10048 error
+            serverSock.bind((tcp_server.HOST,tcp_server.PORT))
+            serverSock.listen(5)
+            print 'TCP Server Activate'
 
-        connectionSock,addr=serverSock.accept() #waiting connect
-        print 'TCP Server Connected'
-        
-        while not rospy.is_shutdown():
-            try :
-                # tcp_server.camera_check = False
-
-                # connectionSock.settimeout(5)
-                buffer=connectionSock.recv(tcp_server.BUFFER)
-                # print buf
-                tcp_server.pub.publish(buffer)
-                # tcp_server.finish = True
-                while True :
-                    if tcp_server.finish == True:
+            connectionSock,addr=serverSock.accept() #waiting connect
+            print 'TCP Server Connected'
+            
+            while not rospy.is_shutdown():
+                try :
+                    # tcp_server.camera_check = False
+                    buffer=connectionSock.recv(tcp_server.BUFFER)
+                    # print buf
+                    if (buffer == ""):
+                        print("no connection")
                         break
                     else:
+                        tcp_server.pub.publish(buffer)
+
+                    while True :
+                        if tcp_server.finish == True:
+                            break
+                        else:
+                            pass
+                    if buffer =="0010": 
+                        if tcp_server.finish == True:
+                            connectionSock.send('0011')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0012')
+                    elif buffer == "0020":
+                    
+                        if tcp_server.finish == True:
+                            connectionSock.send('0021')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0022')
+                        
+                    elif buffer == "0030":
+                    
+                        if tcp_server.finish == True:
+                            connectionSock.send('0031')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0032')
+                    elif buffer == "0110":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0111')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0112')
+                    elif buffer == "0120":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0121')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0122')
+                    elif buffer == "0130":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0131')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0132')
+                    elif buffer == "0140":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0141')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0142')
+
+                    elif buffer == "0210":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0211')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0212')
+                    elif buffer == "0220":
+                        
+                        if tcp_server.finish == True:
+                            connectionSock.send('0221')
+                            tcp_server.finish = False
+                        else:
+                            connectionSock.send('0222')
+                    # Add checking joint state Protocol
+                    elif buffer == "1300":  
+                        # sleep(1) #sleep when code is not working  
+                        if tcp_server.check == 0: # error code 
+                            connectionSock.send('1320')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 1: # init pose a
+                            connectionSock.send('1301')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 2: # init pose b
+                            connectionSock.send('1302')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 3: # init pose c
+                            connectionSock.send('1303')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 4: # ready pose 0
+                            connectionSock.send('1311')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 5: # ready pose 90
+                            connectionSock.send('1312')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 6: # ready pose 180
+                            connectionSock.send('1313')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        elif tcp_server.check == 7: # ready pose 270
+                            connectionSock.send('1314')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                        else: # protocol error 
+                            connectionSock.send('1321')
+                            tcp_server.finish = 0
+                            tcp_server.finish = False
+                    elif buffer =="1100": # server health check 
+                        connectionSock.send('1101')
+                        tcp_server.finish = False
+                    elif buffer =="1200": # camera health check 
+                        if tcp_server.camera_health_check() == True:
+                            connectionSock.send('1201')
+                        else:
+                            connectionSock.send('1202')
+
+                        tcp_server.finish = False
+                    elif buffer =="1000": # reset all
+                        connectionSock.send('1001')
+                        os.system("sudo systemctl restart bringup_ros.service")
+
+                        tcp_server.finish = False
+                    else:
+                        # connectionSock.send('0321')
+
                         pass
-                if buffer =="0010": 
-                    if tcp_server.finish == True:
-                        connectionSock.send('0011')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0012')
-                elif buffer == "0020":
-                   
-                    if tcp_server.finish == True:
-                        connectionSock.send('0021')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0022')
-                    
-                elif buffer == "0030":
-                 
-                    if tcp_server.finish == True:
-                        connectionSock.send('0031')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0032')
-                elif buffer == "0110":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0111')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0112')
-                elif buffer == "0120":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0121')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0122')
-                elif buffer == "0130":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0131')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0132')
-                elif buffer == "0140":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0141')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0142')
-
-                elif buffer == "0210":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0211')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0212')
-                elif buffer == "0220":
-                    
-                    if tcp_server.finish == True:
-                        connectionSock.send('0221')
-                        tcp_server.finish = False
-                    else:
-                        connectionSock.send('0222')
-                # Add checking joint state Protocol
-                elif buffer == "1300":  
-                    # sleep(1) #sleep when code is not working  
-                    if tcp_server.check == 0: # error code 
-                        connectionSock.send('1320')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 1: # init pose a
-                        connectionSock.send('1301')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 2: # init pose b
-                        connectionSock.send('1302')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 3: # init pose c
-                        connectionSock.send('1303')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 4: # ready pose 0
-                        connectionSock.send('1311')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 5: # ready pose 90
-                        connectionSock.send('1312')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 6: # ready pose 180
-                        connectionSock.send('1313')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    elif tcp_server.check == 7: # ready pose 270
-                        connectionSock.send('1314')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                    else: # protocol error 
-                        connectionSock.send('1321')
-                        tcp_server.finish = 0
-                        tcp_server.finish = False
-                elif buffer =="1100": # server health check 
-                    connectionSock.send('1101')
-                    tcp_server.finish = False
-                elif buffer =="1200": # camera health check 
-                    if tcp_server.camera_health_check() == True:
-                        connectionSock.send('1201')
-                    else:
-                        connectionSock.send('1202')
-
-                    tcp_server.finish = False
-                elif buffer =="1000": # reset all
-                    connectionSock.send('1001')
-                    os.system("sudo systemctl restart bringup_ros.service")
-
-                    tcp_server.finish = False
-                else:
-                    # connectionSock.send('0321')
-
-                    pass
-                    
-            except socket.timeout:
-                print 'time out'
-            
-            tcp_server.RATE.sleep()
-        print "exit"
-        connectionSock.close()
-    except Exception as e:
-        rospy.logerr(e)
-    finally:
-        pass
+                        
+                except socket.timeout:
+                    print 'time out'
+                except socket.error:
+                    print ("socket error")
+                    break
+                
+                tcp_server.RATE.sleep()
+            print "exit"
+            connectionSock.close()
+        except Exception as e:
+            rospy.logerr(e)
+        finally:
+            pass
